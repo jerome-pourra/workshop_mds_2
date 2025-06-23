8c6a498fb343d3569039f2284d472fee5b7bac60
StarFlo974 <Starnorf.pro@gmail.com>
2025-06-23 14:39:20 +0200

    feat: ajout du loader

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,1 +1,8 @@
 import './style.css'
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("app").style.display = "block";
+  }, 3000);
+});
