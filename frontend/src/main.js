df1f92b17c4012815c0054b397190b0d8b40e8cb
StarFlo974 <Starnorf.pro@gmail.com>
2025-06-23 16:11:31 +0200

    feat: ajout page appel

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,3 +6,24 @@ window.addEventListener('load', () => {
     document.getElementById("app").style.display = "block";
   }, 3000);
 });
+
+const muteButton = document.getElementById('mute-button');
+const micOn = muteButton.querySelector('.mic-on');
+const micOff = muteButton.querySelector('.mic-off');
+
+let isMuted = true;
+
+// Afficher mic-off par défaut
+micOff.classList.add('visible');
+
+muteButton.addEventListener('click', () => {
+  isMuted = !isMuted;
+
+  if (isMuted) {
+    micOn.classList.remove('visible');
+    micOff.classList.add('visible');
+  } else {
+    micOff.classList.remove('visible');
+    micOn.classList.add('visible');
+  }
+});
